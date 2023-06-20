2f0e8c22979ca24a92492c801f834bce9a6d4e39
Tuna <vhtuananh020402@gmail.com>
2023-06-20 16:57:43 +0700

    finalize super qr

--- superbrain/games/superQr/assets/scripts/main.js
+++ superbrain/games/superQr/assets/scripts/main.js
@@ -9,3 +9,24 @@ function checkAnswer() {
     }
     document.getElementById("answer").innerHTML = text;
 }
+
+function checkInputs() {
+    var inputs = document.getElementsByTagName("input");
+    var values = [];
+  
+    // Get the values of the input fields
+    for (var i = 0; i < inputs.length; i++) {
+      values.push(inputs[i].value);
+    }
+  
+    // Check if all inputs are correct
+    if (values.includes("13") &&
+        values.includes("38") &&
+        values.includes("53") &&
+        values.includes("75") &&
+        values.includes("10")) {
+      document.getElementById("result").textContent = "All are correct!";
+    } else {
+      document.getElementById("result").textContent = "Wrong answers!";
+    }
+  }
