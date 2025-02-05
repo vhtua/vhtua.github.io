489d2ccc4d4f79f9ade0506300e5388576f21113
vhtua <vhtuananh020402@gmail.com>
2025-02-06 01:25:49 +0700

    add: hanoi section

---
+++ hanoi/map_data.js
@@ -0,0 +1,23 @@
+var locations = 
+[   
+    {
+        lat: 21.03160360296427, 
+        lng: 105.81279591265658,
+        ggmaps: "https://maps.app.goo.gl/UtzSzmh1QsN3xC7f9",
+        date: "18:44 04/12/2024",
+        name: "Lotte Center Hanoi 🏬",
+        img: "img/20241204-gaumeo.jpg",
+        desc: "Mình mới lên HN và tới văn phòng làm được 2 ngày. Ở HN mình có cảm giác lạ lẫm nhưng lại rất quen thuộc. Tháng 12 này đang là mùa đông, trời rất là lạnh luôn. Nhưng mà hôm nay mình được lần đầu chở đi cháy phố. Nói là tráy thế thui chứ thực ra là được chở về nhà. À còn một điều quan trọng là mình được tặng một món quà rất là dễ thương nữaa. Mình sẽ gìn giữ món quà này thật cẩn thận."
+    },
+
+    { 
+        lat: 20.394980641181245, 
+        lng: 106.49548423812087,
+        ggmaps: "https://maps.app.goo.gl/aLSMBCPUcw4kGaaS8",
+        date: "14:31 27/01/2025",
+        name: "Chợ Hoa tươi huyện Tiền Hải 🌻", 
+        img: "img/20250127-hoathanhlieu.png", 
+        desc: "Sắp đến Tết rùi, cần phải đi mua gì đó cho gia đình chớ nhỉ. Ở đây mình biết được thêm các loại hoa mới, hoa thanh liễu, hoa phăng các kiểu luôn màa.<br>Cảm ơn nhìu lắmm. <br> Hình dưới là thành quả chúng ta chọn hoa nè." 
+    }
+
+];
