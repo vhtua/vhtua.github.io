d6013e318d20cc34feb881f54a381090b6abe5ae
vhtua <vhtuananh020402@gmail.com>
2025-02-06 08:16:30 +0700

    add: hanoi - 58/3 tran binh

--- hanoi/map_data.js
+++ hanoi/map_data.js
@@ -10,12 +10,22 @@ var locations =
         desc: "Mình mới lên HN và tới văn phòng làm được 2 ngày. Ở HN mình có cảm giác lạ lẫm nhưng lại rất quen thuộc. Tháng 12 này đang là mùa đông, trời rất là lạnh luôn. Nhưng mà hôm nay mình được lần đầu chở đi cháy phố. Nói là tráy thế thui chứ thực ra là được chở về nhà. À còn một điều quan trọng là mình được tặng một món quà rất là dễ thương nữaa. Mình sẽ gìn giữ món quà này thật cẩn thận."
     },
 
+    {
+        lat: 21.033546553526204, 
+        lng: 105.77673667714092,
+        ggmaps: "https://maps.app.goo.gl/sH2wy4Bu52gVQxE77",
+        date: "19:00 10/12/2024",
+        name: "58/3 Trần Bình 🏠",
+        img: "",
+        desc: ""
+    },
+
     { 
         lat: 20.394980641181245, 
         lng: 106.49548423812087,
         ggmaps: "https://maps.app.goo.gl/aLSMBCPUcw4kGaaS8",
         date: "14:31 27/01/2025",
-        name: "Chợ Hoa tươi huyện Tiền Hải 🌻", 
+        name: "Chợ Hoa huyện Tiền Hải 🌻", 
         img: "img/20250127-hoathanhlieu.png", 
         desc: "Sắp đến Tết rùi, cần phải đi mua gì đó cho gia đình chớ nhỉ. Ở đây mình biết được thêm các loại hoa mới, hoa thanh liễu, hoa phăng các kiểu luôn màa.<br>Cảm ơn nhìu lắmm. <br> Hình dưới là thành quả chúng ta chọn hoa nè." 
     }
