8f33c1beaf2ce1c6cd6b51680f8efa82f991108c
vhtua <vhtuananh020402@gmail.com>
2025-02-07 08:37:34 +0700

    add: hanoi - more places

--- hanoi/map_data.js
+++ hanoi/map_data.js
@@ -1,6 +1,7 @@
 var locations = 
 [   
     {
+        id: 1,
         lat: 21.03160360296427, 
         lng: 105.81279591265658,
         ggmaps: "https://maps.app.goo.gl/UtzSzmh1QsN3xC7f9",
@@ -11,16 +12,92 @@ var locations =
     },
 
     {
+        id: 2,
         lat: 21.033546553526204, 
         lng: 105.77673667714092,
         ggmaps: "https://maps.app.goo.gl/sH2wy4Bu52gVQxE77",
-        date: "19:00 10/12/2024",
+        date: "22:00 10/12/2024",
         name: "58/3 Trần Bình 🏠",
-        img: "",
-        desc: ""
+        img: "img/20241210-comngon.jpg",
+        desc: "Nay mình tập văn nghệ YEP tại công ty muộn mới về. Về nhà được hỏi là có muốn ăn cơm không, tấc nhiên là có roài. Thế là sau đó được cho 1 bát cơm thế này nè, ahihi."
     },
 
-    { 
+    {
+        id: 3,
+        lat: 21.03453056879763,  
+        lng: 105.782795321752,
+        ggmaps: "https://maps.app.goo.gl/4K8oWDU6dgKJcLwq7",
+        date: "11:59 14/12/2024",
+        name: "Quán Bún Vịt Quay 🍜",
+        img: "img/20241214-quanvitquay.png",
+        desc: "Chúng mình đi ăn bún vịt nướng ở đây do nghe lói trên top top quán rất là nổi tiếng và ngonn. Đây cũng là lần đầu tiên về HN đi ăn cùng nhao và rất lâu ròi mới thấy lại mặt nè (trước đó toàn thấy đeo khẩu trang khôngg). Vừa ăn vừa lói phét rất nhiều chuyện trên trời, mình thì cười hơ hớ hơi bị to (cảm thấy bản thân hơi vô duyên tẹoo)."
+    },
+
+    {
+        id: 4,
+        lat: 21.014974615145693, 
+        lng: 105.81223476216452,  
+        ggmaps: "https://maps.app.goo.gl/GQwmtFSB1DGFbrC58",
+        date: "13:12-18:04 14/12/2024",
+        name: "Quán Coffee có mèo ngu ☕",
+        img: "img/20241214-quancoffeestudydate.jpg",
+        desc: "Sau khi ăn xong bún vịt nướng, chúng mình đi tiếp tầm 7km đến quán cà phê này. Hoa nói là chỗ này thích hợp để chill, chụp ảnh các thứ và quan trọng nhất là để học. 🐥 Ừ đúng là để học thiệc, chúng mình sau khi nghịch ngợm chụp ảnh xong thì đứa bé này mới nhớ ra là cần support 1 thanh niên thi tiếng Anh. Rồi sau đó cả 2 đứa lao vào lòi mắt nhìn cái đề thằng bé chụp (chao ôi mắt toai) và support nó. Mình gáy là 1 IELTS và 1 TOEIC ở đây thì đúng là chúng mình sẽ công phá mọi đề!!! Và sau đó vì quá mệt mỏi nên chúng mình quyết định hỏi ChatGPT... <br>Sau cùng, mình giới thiệu cho Hoa về IELTS Listening và để cho đứa bé này tập trung làm bài (mãi mới đúng mục đích đến quán này)."
+    },
+
+    {
+        lat: 21.036761726662636, 
+        lng: 105.84770172478342,  
+        ggmaps: "https://maps.app.goo.gl/VfNM6sFhXJAS8Xd6A",
+        date: "18:19 14/12/2024",
+        name: "Phố Hàng Mã ☃️",
+        img: "img/20241214-nph.JPG",
+        desc: "Hà Nội trong cái lạnh của mùa đông, các hàng quán phố Hàng Mã bán đồ trang trí Giáng Sinh đầy đủ sắc vàng, xanh, đỏ..., tất cả tạo nên một không khí thật ấm cúng. Đây là lần đầu mình biết đi chơi Giáng Sinh là như thế nào, chúng mình sẽ đi qua từng quán, chụp ảnh, ngắm đồ (cũm muốn mua 1 số thứ lắm cơ mà sợ mắc), rùi tất nhiên là sẽ lói phét và lạc đường một chút. Nghe hơi ngốc nhưng mà vui lémm. Hãy luôn cười thật nhèo nhéee."
+    },
+
+
+    {
+        lat: 21.02439296914177, 
+        lng: 105.840952090882,
+        ggmaps: "https://maps.app.goo.gl/mjNcmgFBPNaFxMsd8",
+        date: "19:42 14/12/2024",
+        name: "Ga Hà Nội 🚉",
+        img: "img/20241214-gahanoi.jpg",
+        desc: "Đi 1 hồi Giáng Sinh ở Hàng Mã đói quá nên là tụi mình tạm biệt Hàng Mã và vác xe đi kiếm quán ăn. Trên đoạn đường đó thì vô tình đi qua Ga Hà Nội, mình thấy đẹp nên dừng xe lại để cả 2 đứa chụp ảnh."
+    },
+
+    {
+        lat: 21.007734177098854, 
+        lng: 105.83258263524807, 
+        ggmaps: "https://maps.app.goo.gl/oow2neKAcZH5ebsw9",
+        date: "19:57 14/12/2024",
+        name: "Nem nướng Nha Trang- Bún Đậu Mắm Tôm 🍽️",
+        img: "img/20241214-quannemnuong.jpg",
+        desc: "Trên đường trở về từ Ga Hà Nội mà trước hơn đó là Giáng Sinh ở Hàng Mã. Hoa có tra ra chỗ này được đánh giá tích cực với số điểm rất cao trên Google Maps. Thế là không nghĩ nhèo, 2 đứa vào gọi món ăn luôn cho lóng. Mình thì thấy nước chấm bên này có mùi vị cũng rất thú vị và buồn cười nên vô tình đã khiến cho cả 2 có mấy trò lói phét cười ằng ặc."
+    },
+
+    {
+        lat: 21.004251017193297, 
+        lng: 105.83126645063315, 
+        ggmaps: "https://maps.app.goo.gl/QNQ29sJ7NrF1R7Bd6",
+        date: "20:55 14/12/2024",
+        name: "Ngõ 1A Tôn Thất Tùng 🚩",
+        img: "img/20241214-tonthattung.jpg",
+        desc: "Đây là nơi đi chơi Giáng Sinh phù hợp với tụi sinh viên mới ra trường ít xèng như chúng mềnh. Hoa hỏi mình có biết tại sao Hoa lại biết chỗ này không. Nhìn con bé khi nói xong câu đấy cũng buồn, đúng là một nhịp hẫng trong tim con bé. Mới giờ này năm ngoái hai đứa nó từng rất đẹp đôi, đúng là cuộc sống luôn thử thách mỗi chúng ta. Dù sao thì sau đấy chúng mình lại đi khắp các hàng quán, ở đây mình đã tìm được đồ cần mua. Mình chọn tặng con bé cái đấm lưng có hình con cừu (theo Hoa mô tả là như thía) và 2 cái kéo khóa cho cái ba lô của mềnh."
+    },
+
+    {
+        lat: 21.04143631118294, 
+        lng: 105.76520343043093,
+        ggmaps: "https://maps.app.goo.gl/TWau74WnjTs31V149",
+        date: "22:00 10/12/2024",
+        name: "58/3 Trần Bình 🏠",
+        img: "img/20241210-comngon.jpg",
+        desc: "Nay mình tập văn nghệ YEP tại công ty muộn mới về. Về nhà được hỏi là có muốn ăn cơm không, tấc nhiên là có roài. Thế là sau đó được cho 1 bát cơm thế này nè, ahihi."
+    },
+
+
+    {
+        id: 10000000,
         lat: 20.394980641181245, 
         lng: 106.49548423812087,
         ggmaps: "https://maps.app.goo.gl/aLSMBCPUcw4kGaaS8",
