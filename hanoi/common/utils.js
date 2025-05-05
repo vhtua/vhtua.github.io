48fcb4a8df4d3a69783693f9e79e604675715d6e
Vu Hoang Tuan Anh <73384816+vhtua@users.noreply.github.com>
2025-05-05 07:06:34 +0000

    add future msg

--- hanoi/common/utils.js
+++ hanoi/common/utils.js
@@ -10,3 +10,23 @@ function getRandomNumberInRangeExcept(min, max, excluded) {
   
     return randomNumber;
 }
+
+
+// Encode a string to Base64
+function encodeString(str) {
+  return btoa(unescape(encodeURIComponent(str)));
+}
+
+// Decode a Base64 string
+function decodeString(encodedStr) {
+  return  decodeURIComponent(escape(atob(encodedStr)));
+}
+
+// Example usage
+// const original = `Chúc mừng cậu đã tìm được đến nơi sâu thẳm nhất của web map này`;
+// const encoded = encodeString(original);
+// const decoded = decodeString(encoded);
+
+// console.log("Original:", original);
+// console.log("Encoded:", encoded);
+// console.log("Decoded:", decoded);
